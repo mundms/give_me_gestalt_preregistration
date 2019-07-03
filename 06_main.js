4020b664770e903b85eaf6a69ec01a67997e89f3
mundms <46026391+mundms@users.noreply.github.com>
2019-07-03 22:46:19 +0200

    changed back to debug

--- 06_main.js
+++ 06_main.js
@@ -39,7 +39,7 @@ $("document").ready(function() {
             // Possible deployment methods are:
             // "debug" and "directLink"
             // As well as "MTurk", "MTurkSandbox" and "Prolific"
-            deployMethod: "directLink",
+            deployMethod: "debug",
             contact_email: "YOUREMAIL@wherelifeisgreat.you",
             prolificURL: "https://app.prolific.ac/submissions/complete?cc=SAMPLE1234"
         },
