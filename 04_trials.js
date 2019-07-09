b201610106d2bcf026334fb42d89ecb668fd585a
mundms <46026391+mundms@users.noreply.github.com>
2019-07-09 16:24:09 +0200

    Cubist to cubist

--- 04_trials.js
+++ 04_trials.js
@@ -2,7 +2,7 @@
 const trial_info = {
     expert: [
         {
-            question: "Do you consider yourself an <strong>expert in Cubist art</strong> or were ever engaged in any kind of Cubist art education?",
+            question: "Do you consider yourself an <strong>expert in cubist art</strong> or were ever engaged in any kind of cubist art education?",
             option1: "Yes",
             option2: "No"
         },
